f98a41a4b6c0c91a4dc964c280b577c489c832e6
Matthew J Singer <matt@bartaste.com>
2012-12-06 16:11:43 -0500

    Commit One Month Rails to Section 5.4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '13f19a89a19a308a295f1c68ec34087841d7169e678024af17243d8db2f4d5d20e66fa08e638145d3795a27dc44a78936daefcb4de851972765b46f3e1305c16'
