c9b56b165a91429660e2788cedf36063915878ed
Matthew J Singer <matt@bartaste.com>
2013-01-07 12:19:11 -0500

    fix confirm linkback III

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => 'smtp.sendgrid.net',
-  :port                 => 587,
-  :domain               => 'http://whispering-savannah-8378.herokuapp.com',
-  :user_name            => ENV['SENDGRID_USERNAME'],
-  :password             => ENV['SENDGRID_PASSWORD'],
-  :authentication       => :plain,
-  :enable_starttls_auto => true
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
+ActionMailer::Base.delivery_method = :smtp
