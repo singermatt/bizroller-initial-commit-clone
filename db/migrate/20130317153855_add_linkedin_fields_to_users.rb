50470354e00c0d4dcb5b04ee22800ca148354c8e
singermatt <matt@bartaste.com>
2013-03-17 13:38:23 -0400

    Add LinkedIn Fields to database

---
+++ db/migrate/20130317153855_add_linkedin_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddLinkedinFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
