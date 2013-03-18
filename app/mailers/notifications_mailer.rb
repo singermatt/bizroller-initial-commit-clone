b73e5e6635bf932e22a393044022d5db857f97e8
singermatt <matt@bartaste.com>
2013-03-18 15:16:41 -0400

    Contact Form Mail From

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationsMailer < ActionMailer::Base
 
-  default :from => "noreply@whispering-savannah-8378.herokuapp.com"
+  default :from => "bizroller.com"
   default :to => "beta@bizroller.com"
 
   def new_message(message)
